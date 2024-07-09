312c681700521f56bb70674a8a4b3bb25d5d5191
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2024-07-09 08:15:23 +0300

    packages and imports

---
+++ PackagesAndImports/src/App.java
@@ -0,0 +1,10 @@
+import java.util.*; // import all classes in java.util package
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+}
---
+++ PackagesAndImports/src/dev/lpa/Main.java
@@ -0,0 +1,13 @@
+package dev.lpa;
+
+// import com.abc.first.Item;
+
+public class Main {
+  public static void main(String[] args) {
+    // Item first = new Item("Burger");
+
+    com.abc.first.Item first = new com.abc.first.Item("Burger");
+
+    System.out.println(first);
+  }
+}
